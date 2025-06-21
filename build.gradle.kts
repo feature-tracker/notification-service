c9adb993dc8c6decbccb1c0351e8666e90e0ebb1
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-06-21 08:04:59 +0530

    chore: upgrade to spring boot 3.5.3
    Signed-off-by: K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.2"
+    id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.gorylenko.gradle-git-properties") version "2.5.0"
     id("com.diffplug.spotless") version "7.0.4"
