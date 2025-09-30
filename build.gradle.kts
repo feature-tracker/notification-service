f06c5dc3c8627219ef6fbfa8943a93671c41fc66
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-09-30 08:54:03 +0530

    chore: upgrade deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.3"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.spring") version "2.2.20"
+    id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.gorylenko.gradle-git-properties") version "2.5.0"
-    id("com.diffplug.spotless") version "7.0.4"
+    id("com.gorylenko.gradle-git-properties") version "2.5.3"
+    id("com.diffplug.spotless") version "8.0.0"
 }
 
 group = "com.sivalabs.ft"
@@ -19,7 +19,7 @@ var dockerImageName = "sivaprasadreddy/ft-notification-service"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(24)
     }
 }
 
